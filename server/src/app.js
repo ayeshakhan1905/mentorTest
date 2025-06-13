42458a78e1b52c62c9829a02284f8298d472ba7d
ayeshakhan1905 <ayesha.khan.ak1905@gmail.com>
2025-06-13 22:02:38 +0530

    done2

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,39 @@
-const express = require("express")
-const app = express()
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://mentor-test-tm3h.vercel.app'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use(cookieParser())
 
-const userRoutes = require("./router/authRoutes")
-app.use("/api/users", userRoutes)
+app.use(cookieParser());
 
-module.exports = app
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:5173', 'https://mentor-test-tm3h.vercel.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
+
+const userRoutes = require("./router/authRoutes");
+app.use("/api/users", userRoutes);
+
+module.exports = app;
