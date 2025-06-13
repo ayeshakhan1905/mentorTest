6d6215fbd5ea15288fa5bc67296429ce48d2d9cf
ayeshakhan1905 <ayesha.khan.ak1905@gmail.com>
2025-06-14 00:08:25 +0530

    updated links

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -4,7 +4,7 @@ const isDev = import.meta.env.MODE === "development";
 const axiosInstance = axios.create({
   baseURL: isDev
     ? "http://localhost:3000/api"
-    : "https://mentortest-3af8.onrender.com/api",
+    : "https://mentortest-sgdb.onrender.com/api",
   withCredentials: true,
 });
 
