654376e09b5aa2c7a137b8ec5978fdbc32bc6037
ayeshakhan1905 <ayesha.khan.ak1905@gmail.com>
2025-06-13 22:41:54 +0530

    done3

--- server/src/app.js
+++ server/src/app.js
@@ -4,33 +4,35 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const allowedOrigins = ['http://localhost:5173', 'https://mentor-test-tm3h.vercel.app'];
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://mentor-test-tm3h.vercel.app'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
+
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5173', 'https://mentor-test-tm3h.vercel.app'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
-});
-
-app.options('*', (req, res) => {
-  res.sendStatus(200);
 });
 
 const userRoutes = require("./router/authRoutes");
