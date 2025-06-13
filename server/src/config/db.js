0d7218c31f6162108d730594abe423662680684a
ayeshakhan1905 <ayesha.khan.ak1905@gmail.com>
2025-06-14 01:41:47 +0530

    lasttt

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,16 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB  = async()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.log("MongoDB connected successfully")
+const connectDB = async () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      tlsAllowInvalidCertificates: false,
     })
-}
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    });
+};
 
 module.exports = connectDB;
